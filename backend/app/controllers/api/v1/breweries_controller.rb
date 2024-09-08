d1d583b2a52e73b5ff45c66b6f8b3bf3837f032b
LHeuze <lheuze@miuandes.cl>
2024-09-08 18:46:03 -0300

    arreglo cagada

---
+++ backend/app/controllers/api/v1/breweries_controller.rb
@@ -0,0 +1,16 @@
+class API::V1::BreweriesController < ApplicationController
+    respond_to :json
+    include Authenticable
+  
+    def show
+      @beer = Beer.includes(brand: :brewery).find(params[:id])
+      @bars = Bar.joins(:bars_beers).where(bars_beers: { beer_id: @beer.id })
+    
+      render json: {
+        beer: @beer,
+        brewery: @beer.brand.brewery,
+        bars: @bars
+      }
+    end
+  end
+    
---
+++ backend/app/controllers/api/v1/countries_controller.rb
@@ -0,0 +1,9 @@
+class API::V1::CountriesController < ApplicationController
+    respond_to :json
+    include Authenticable
+    def index
+      countries = Country.all
+      render json: countries
+    end
+  end
+    
